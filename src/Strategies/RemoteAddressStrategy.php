eb2607d8b94279ad26f12a3b4b323fda82439795
Mike Francis <mikeffrancis@gmail.com>
2019-10-25 09:29:12 +0100

    chore(deps): Updated dependencies for Laravel 6 (#1)

--- src/Strategies/RemoteAddressStrategy.php
+++ src/Strategies/RemoteAddressStrategy.php
@@ -3,13 +3,14 @@
 namespace MikeFrancis\LaravelUnleash\Strategies;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use MikeFrancis\LaravelUnleash\Strategies\Contracts\Strategy;
 
 class RemoteAddressStrategy implements Strategy
 {
   public function isEnabled(array $params, Request $request): bool
   {
-    $remoteAddresses = explode(',', array_get($params, 'remoteAddress', ''));
+    $remoteAddresses = explode(',', Arr::get($params, 'remoteAddress', ''));
 
     if (count($remoteAddresses) === 0) {
       return false;
